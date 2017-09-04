3c3bd16bd7c100ec131c3e9f7719ff1dec4300b3
Zsolt Fekete <nabunub@hotmail.com>
2017-09-04 18:30:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ date_default_timezone_set('Europe/Budapest');
 	<a id="githubribbon" href="https://github.com/nXu/indadl"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>
 		<div id="wrapper" class="top-margin centerize">
 			<h1><a href=".">Indavideó letöltés</a></h1>
+			<h3><a href="https://indavideo-new.nxu.hu/">Kattints ide és próbáld ki az új letöltőt</a></h3>
 			<div id="content">
 				<div id="result_page">
 					<p id="back"><a id="back_link" href="#">Új videó letöltése</a></p>
