8bc03d1e9639ec4c1585391b9fa9fbd8cfaa6dbf
zsolt <fzs@fwmail.hu>
2020-09-19 13:00:08 +0200

    Update tests

--- tests/Feature/IndavideoClientTest.php
+++ tests/Feature/IndavideoClientTest.php
@@ -26,27 +26,47 @@ class IndavideoClientTest extends TestCase
         $client->getVideoUrl('https://www.google.com');
     }
 
-    public function testItReturnsValidUrlForBasicIndavideoUrl()
+    public function testItReturnsMultipleResolutionsForBasicUserVideo()
     {
         $client = new IndavideoClient(new Client());
-        $url = $client->getVideoUrl('http://indavideo.hu/video/Anime_cosplay_bemutato');
+        $urls = $client->getVideoUrl('https://indavideo.hu/video/Jatek_kezenallva');
 
-        $this->assertNotFalse(curl_init($url));
+        $this->assertNotFalse(curl_init($urls->getSd()));
+        $this->assertNotFalse(curl_init($urls->getHd()));
     }
 
-    public function testItReturnsValidUrlForSudomainIndavideoUrl()
+    public function testItReturnsMultipleResolutionsForProUserVideo()
     {
         $client = new IndavideoClient(new Client());
-        $url = $client->getVideoUrl('http://index.indavideo.hu/video/rasko_eszter_szaboz_sziget_25_eves');
+        $urls = $client->getVideoUrl('https://indavideo.hu/video/Batman_-_10_erdekes_teny');
 
-        $this->assertNotFalse(curl_init($url));
+        $this->assertNotFalse(curl_init($urls->getSd()));
+        $this->assertNotFalse(curl_init($urls->getHd()));
+    }
+
+    public function testItReturnsMultipleResolutionsForProUserAnime()
+    {
+        $client = new IndavideoClient(new Client());
+        $urls = $client->getVideoUrl('https://indavideo.hu/video/Muhyo_to_Rouji_no_Maho_uritsu_Soudan_Jimusho_2evad_5resz_Magyar_Felirattal');
+
+        $this->assertNotFalse(curl_init($urls->getSd()));
+        $this->assertNotFalse(curl_init($urls->getHd()));
+    }
+
+    public function testItReturnsValidUrlsForSudomainIndavideoUrl()
+    {
+        $client = new IndavideoClient(new Client());
+        $urls = $client->getVideoUrl('http://index.indavideo.hu/video/rasko_eszter_szaboz_sziget_25_eves');
+
+        $this->assertNotFalse(curl_init($urls->getSd()));
+        $this->assertNotFalse(curl_init($urls->getHd()));
     }
 
     public function testItReturnsValidUrlForEmbedUrls()
     {
         $client = new IndavideoClient(new Client());
-        $url = $client->getVideoUrl('http://embed.indavideo.hu/player/video/691b51997a');
+        $urls = $client->getVideoUrl('http://embed.indavideo.hu/player/video/691b51997a');
 
-        $this->assertNotFalse(curl_init($url));
+        $this->assertNotFalse(curl_init($urls->getSd()));
     }
 }
