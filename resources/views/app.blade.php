37ae3a806846131fb4a292d686facca6b17cc79b
zsolt <fzs@fwmail.hu>
2020-09-21 19:47:58 +0200

    Remove fontawesome

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,13 +9,6 @@
     <meta name="description" content="Ingyenes, egyszerű Indavideó letöltő, akár mobilon is. Videók letöltése egy kattintással mp4 formátumban.">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <link rel="preload"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-          as="style"
-          onload="this.onload=null;this.rel='stylesheet'"
-          integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw=="
-          crossorigin="anonymous"
-    >
-    <link rel="preload"
           href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap"
           as="style"
           onload="this.onload=null;this.rel='stylesheet'"
@@ -45,7 +38,21 @@
 
                     <div class="button-container">
                         <a class="button" href="#" v-on:click="getSource">
-                            <span v-if="loading"><i class="fa fa-fw fa-spin fa-circle-o-notch"></i></span>
+                            <span v-if="loading">
+                                <svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                     viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve" style="width:20px;height:20px;">
+                                    <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+                                      <animateTransform
+                                          attributeName="transform"
+                                          attributeType="XML"
+                                          type="rotate"
+                                          dur="1s"
+                                          from="0 50 50"
+                                          to="360 50 50"
+                                          repeatCount="indefinite" />
+                                  </path>
+                                </svg>
+                            </span>
                             <span v-else>Letöltés</span>
                         </a>
                     </div>
@@ -95,7 +102,8 @@
 
         <p>
             <a href="https://www.paypal.me/nabunub" target="_blank" class="button donate" rel="noreferrer">
-                <i class="fa fa-fw fa-paypal"></i> DONATE
+                <svg style="height:12px;width:12px;display:inline-block;vertical-align:middle" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="paypal" class="svg-inline--fa fa-paypal fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M111.4 295.9c-3.5 19.2-17.4 108.7-21.5 134-.3 1.8-1 2.5-3 2.5H12.3c-7.6 0-13.1-6.6-12.1-13.9L58.8 46.6c1.5-9.6 10.1-16.9 20-16.9 152.3 0 165.1-3.7 204 11.4 60.1 23.3 65.6 79.5 44 140.3-21.5 62.6-72.5 89.5-140.1 90.3-43.4.7-69.5-7-75.3 24.2zM357.1 152c-1.8-1.3-2.5-1.8-3 1.3-2 11.4-5.1 22.5-8.8 33.6-39.9 113.8-150.5 103.9-204.5 103.9-6.1 0-10.1 3.3-10.9 9.4-22.6 140.4-27.1 169.7-27.1 169.7-1 7.1 3.5 12.9 10.6 12.9h63.5c8.6 0 15.7-6.3 17.4-14.9.7-5.4-1.1 6.1 14.4-91.3 4.6-22 14.3-19.7 29.3-19.7 71 0 126.4-28.8 142.9-112.3 6.5-34.8 4.6-71.4-23.8-92.6z"></path></svg>
+                <span style="display-inline-block;vertical-align:middle">DONATE</span>
             </a>
         </p>
     </footer>
