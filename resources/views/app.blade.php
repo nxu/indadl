dce35deaf691c5cbb26126d78d56faaa5d3f993f
zsolt <fzs@fwmail.hu>
2020-09-21 19:18:42 +0200

    Frontend optimizations

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,13 @@
     <title>Indavideó letöltés</title>
     <meta name="description" content="Ingyenes, egyszerű Indavideó letöltő, akár mobilon is. Videók letöltése egy kattintással mp4 formátumban.">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="preload"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          as="style"
+          onload="this.onload=null;this.rel='stylesheet'"
+          integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw=="
+          crossorigin="anonymous"
+    >
 </head>
 <body>
 
